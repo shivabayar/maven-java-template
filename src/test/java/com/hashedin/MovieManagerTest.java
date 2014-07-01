88a297c877bd8f064eddab4d86ccbd04b87d232f
shiva <shivraj.s@hashedin.com>
2014-07-01 19:07:57 +0530

    wrote test cases

--- src/test/java/com/hashedin/MovieManagerTest.java
+++ src/test/java/com/hashedin/MovieManagerTest.java
@@ -3,9 +3,24 @@ package com.hashedin;
 import java.io.InputStream;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 
-public class MovieManagerTest {
+public class MovieManagerTest extends TestCase {
 	
 	
-
+	 public void testApp()
+	    {
+	    	MoviesManager movieManager = new MoviesManager();
+	    	RatingsManager ratingsManager = new RatingsManager();
+	    	
+	    	InputStream movieInputStream = movieManager.readDataFile("movies_test.data");
+	    	
+//	    	movieManager.convertInputStreamToMap(movieInputStream);
+//	    	
+//	    	InputStream ratingsInputStream = ratingsManager.readDataFile("ratings.data");
+	    	
+//	        assertEquals( movieManager.convertInputStreamToMap(movieInputStream).size(),3 );
+	        
+	        assertEquals( movieManager.convertInputStreamToMap(movieInputStream).get(3).getName(),"Four Rooms (1995)" );
+	    }
 }
